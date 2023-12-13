11d8f6c439485fb4d2da6b96653fcb004cb3308a
Folau Kaveinga <folaukaveinga@gmail.com>
2023-12-13 15:40:42 -0700

    jjj

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,12 @@
 import Image from 'next/image'
-import styles from './page.module.css'
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+  console.log("NEXT_PUBLIC_ENV: ", process.env.NEXT_PUBLIC_ENV)
+
+  return (
+    <main className='container'>
+      <h2>Hello</h2>
 
     </main>
   )
