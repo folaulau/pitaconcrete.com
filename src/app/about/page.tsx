26406c978121fdf44c670b401a0892101430e153
Folau Kaveinga <folaukaveinga@gmail.com>
2023-12-13 16:00:19 -0700

    asdf

---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+
+export default function About() {
+
+  console.log("NEXT_PUBLIC_ENV: ", process.env.NEXT_PUBLIC_ENV)
+
+  return (
+    <main className='container'>
+      <h2>About</h2>
+
+    </main>
+  )
+}
