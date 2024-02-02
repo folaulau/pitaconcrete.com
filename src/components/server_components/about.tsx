f3f5ac198e9d474fd81210c3b40f9c01ddcfbe5b
Folau Kaveinga <folaukaveinga@gmail.com>
2024-02-01 17:17:05 -0700

    Update about.tsx

--- src/components/server_components/about.tsx
+++ src/components/server_components/about.tsx
@@ -11,7 +11,7 @@ export default function AboutComp() {
 
                 <div className="row position-relative">
 
-                <div className="col-sm-3">
+                <div className="col-12 col-sm-3">
                     <div className="row">
                         <div className="col-12">
                             <img
@@ -22,51 +22,50 @@ export default function AboutComp() {
                         </div>
                     </div>
                 </div>
-                <div className="col-sm-9">
-                    <div className="container mt-5">
-                        <div className="card">
-                            <div className="card-body">
-                            <h2 className="card-title">My Story</h2>
-                            <p className="card-text">
+                <div className="col-12 col-sm-9">
+                    <div className="card">
+                        <div className="card-body">
+                        <h2 className="card-title">My Story</h2>
+                        <p className="card-text">
 
-                                <div className="row mt-2">
-                                    <div className="col-12">
-                                        My name is Peter Fotu. I have over 18 years of experience in concrete with a focus on providing top quality service.
-                                    </div>
+                            <div className="row mt-2">
+                                <div className="col-12">
+                                    My name is Peter Fotu. I have over 18 years of experience in concrete with a focus on providing top quality service.
                                 </div>
-                                <div className="row mt-2">
-                                    <div className="col-12">
-                                        As a dedicated contractor, there's a unique joy I experience in witnessing our clients' reactions during the final reveal of their project. My top priority is ensuring that the final result not only meets but exceeds our initial agreement, guaranteeing complete satisfaction and happiness for our clients.
-                                    </div>
-                                </div>
-                                <div className="row mt-2">
-                                    <div className="col-12">
-                                        The transformation of a space from start to finish is always remarkable, but what truly motivates me is seeing our clients' faces light up in approval of the work we've done for them. This is the true measure of success for me and my team.
-                                    </div>
-                                </div>
-                                <div className="row mt-2">
-                                    <div className="col-12">
-                                        Our expertise spans a wide range of services, including comprehensive landscaping designs, efficient sprinkler system installations, meticulous tree trimming and removal, as well as skilled demolition, precise framing, and seamless drywall services. We take pride in our versatility and are eager to assist you in bringing your vision to life.
-                                    </div>
-                                </div>
-                                <div className="row mt-2">
-                                    <div className="col-12">
-                                        I take great pride in the quality of our work, and it's my personal mission to ensure that you feel confident and reassured in choosing us for your project needs. Let's work together to create something truly remarkable.
-                                    </div>
-                                </div>
-                    
-                            </p>
-                            <h3 className="mt-4">My Credentials</h3>
-                            <p className="card-text">
-                                License Number: 13609914-5501
-                                <a href="#" className="btn btn-primary btn-sm">verify here</a>
-                            </p>
-                            <p className="card-text">
-                                Insured by <a href="#" className="link-primary">Insurance Company</a>
-                            </p>
                             </div>
+                            <div className="row mt-2">
+                                <div className="col-12">
+                                    As a dedicated contractor, there's a unique joy I experience in witnessing our clients' reactions during the final reveal of their project. My top priority is ensuring that the final result not only meets but exceeds our initial agreement, guaranteeing complete satisfaction and happiness for our clients.
+                                </div>
+                            </div>
+                            <div className="row mt-2">
+                                <div className="col-12">
+                                    The transformation of a space from start to finish is always remarkable, but what truly motivates me is seeing our clients' faces light up in approval of the work we've done for them. This is the true measure of success for me and my team.
+                                </div>
+                            </div>
+                            <div className="row mt-2">
+                                <div className="col-12">
+                                    Our expertise spans a wide range of services, including comprehensive landscaping designs, efficient sprinkler system installations, meticulous tree trimming and removal, as well as skilled demolition, precise framing, and seamless drywall services. We take pride in our versatility and are eager to assist you in bringing your vision to life.
+                                </div>
+                            </div>
+                            <div className="row mt-2">
+                                <div className="col-12">
+                                    I take great pride in the quality of our work, and it's my personal mission to ensure that you feel confident and reassured in choosing us for your project needs. Let's work together to create something truly remarkable.
+                                </div>
+                            </div>
+                
+                        </p>
+                        <h4 className="mt-4">My Credentials</h4>
+                        <p className="card-text">
+                            License Number: 13609914-5501
+                            <a href="#" className="btn btn-primary btn-sm">verify here</a>
+                        </p>
+                        <p className="card-text">
+                            Insured by <a href="#" className="link-primary">Insurance Company</a>
+                        </p>
                         </div>
                     </div>
+                 
 
                 </div>
 
