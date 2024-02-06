56e2a00fc715aad8caa61e0e4f651b56b5d33f9c
Folau Kaveinga <folaukaveinga@gmail.com>
2024-02-06 14:15:24 -0700

    asdfasdf

--- src/components/client_components/AllMediaDisplay.js
+++ src/components/client_components/AllMediaDisplay.js
@@ -151,7 +151,6 @@ export default function AllMediaDisplay() {
                   </div>
                 </div>
                 <div>
-                mediaInfos
                 </div>
                 <div className='row'>
                   {
