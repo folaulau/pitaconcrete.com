8df7936e91ea90149790e2b795551151fd5ddd33
Folau Kaveinga <folaukaveinga@gmail.com>
2024-02-06 13:32:45 -0700

    Update AllMediaDisplay.js

--- src/components/client_components/AllMediaDisplay.js
+++ src/components/client_components/AllMediaDisplay.js
@@ -134,6 +134,7 @@ export default function AllMediaDisplay() {
 
             <div className='row'>
               <div className='col-12 col-sm-12'>
+                <h3>sdfsdf</h3>
                 <div className='row mb-2'>
                   <div className='col-12 col-sm-12'>
                     {
