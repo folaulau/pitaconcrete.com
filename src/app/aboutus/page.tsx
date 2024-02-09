8e00048f86eeb8abea4bb85244be70f484f5351f
Folau Kaveinga <folaukaveinga@gmail.com>
2024-02-09 12:36:01 -0700

    Update page.tsx

--- src/app/aboutus/page.tsx
+++ src/app/aboutus/page.tsx
@@ -81,32 +81,32 @@ export default function AboutUs() {
                                             <div className="row">
                                                 <div className="col-12">
 
-                                                    <div className="row mt-2 p-1">
+                                                    <div className="row mt-2">
                                                         <div className="col-12">
                                                         Hello, I am Peter Fotu. My journey in the world of concrete and construction spans over 18 years, beginning when I was just 16 years old, working alongside my father. This early experience laid the foundation for my deep-rooted passion and understanding of the trade. It taught me not just the skills of the craft, but also the value of hard work, precision, and client satisfaction.
                                                         </div>
                                                     </div>
-                                                    <div className="row mt-2 p-1">
+                                                    <div className="row mt-2">
                                                         <div className="col-12">
                                                         Years later, this path led me to join the Local 500 Union, where I further honed my expertise for 5 years. This experience was pivotal, equipping me with a blend of traditional techniques and innovative practices, setting me apart in the industry.
                                                         </div>
                                                     </div>
-                                                    <div className="row mt-2 p-1">
+                                                    <div className="row mt-2">
                                                         <div className="col-12">
                                                             As a dedicated contractor, there&apos;s a unique joy I experience in witnessing our clients&apos; reactions during the final reveal of their project. My top priority is ensuring that the final result not only meets but exceeds our initial agreement, guaranteeing complete satisfaction and happiness for our clients.
                                                         </div>
                                                     </div>
-                                                    <div className="row mt-2 p-1">
+                                                    <div className="row mt-2">
                                                         <div className="col-12">
                                                             The transformation of a space from start to finish is always remarkable, but what truly motivates me is seeing our clients&apos; faces light up in approval of the work we&apos;ve done for them. This is the true measure of success for me and my team.
                                                         </div>
                                                     </div>
-                                                    <div className="row mt-2 p-1">
+                                                    <div className="row mt-2">
                                                         <div className="col-12">
                                                             Our expertise spans a wide range of services, including comprehensive landscaping designs, efficient sprinkler system installations, meticulous tree trimming and removal, as well as skilled demolition, precise framing, and seamless drywall services. We take pride in our versatility and are eager to assist you in bringing your vision to life.
                                                         </div>
                                                     </div>
-                                                    <div className="row mt-2 p-1">
+                                                    <div className="row mt-2">
                                                         <div className="col-12">
                                                             I take great pride in the quality of our work, and it&apos;s my personal mission to ensure that you feel confident and reassured in choosing us for your project needs. Let&apos;s work together to create something truly remarkable.
                                                         </div>
